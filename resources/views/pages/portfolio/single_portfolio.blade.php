89a83cc95a35a8b029ecb9d8e96819456f6af237
Evgeny <tonitsoi.evgenii@gmail.com>
2018-03-22 16:34:10 +0200

    Change in styles.css and change a single page of the portfolio

--- resources/views/pages/portfolio/single_portfolio.blade.php
+++ resources/views/pages/portfolio/single_portfolio.blade.php
@@ -16,46 +16,63 @@
     <section id="single-content">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center caption-h3">
-                    <h3>Одиночная страница(заголовок)</h3>
+                <div class="col-md-12 top-caption">
+                    <h3>Ул. Рыбацкое, 3-ёх комнатная квартира</h3>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <!--      <div class="well">
-                              <h4>Описание объекта</h4>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at deserunt eligendi nisi numquam provident, sed sint unde voluptates voluptatum! Dolorum, numquam quas.</p>
-                          </div>-->
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><strong>Описание Объекта</strong></div>
-                        <div class="panel-body">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur est illum maiores vel. Accusamus accusantium alias aspernatur aut consequatur dolores dolorum in, iste labore nobis obcaecati sapiente voluptatem voluptates!
-                        </div>
+                <div class="col-md-7 hidden-sm hidden-xs">
+                    <div class="image">
+                        <img src="{{ asset('img/3213.jpg') }}" class="img-responsive" width="600px" alt="">
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <h3 class="text-center object-caption">Характеристика объекта</h3>
+                    <div class="notice notice-info">
+                        <strong>Проект квартиры:</strong> <span class="pull-right">2-ух комнатная</span>
+                    </div>
+                    <div class="notice notice-info">
+                        <strong>Площадь:</strong> <span class="pull-right">150кв.м</span>
+                    </div>
+                    <div class="notice notice-info">
+                        <strong>Вид ремонта:</strong> <span class="pull-right">Евроремонт</span>
+                    </div>
+                    <div class="notice notice-info">
+                        <strong>Год выполнения:</strong> <span class="pull-right">2017г</span>
+                    </div>
+                    <div class="notice notice-info">
+                        <strong>Срок выполнения:</strong> <span class="pull-right">7 месяцев</span>
                     </div>
                 </div>
             </div>
+            <hr>
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
+                <div class="gallery-caption text-center">
+                    <h3>Галерея</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 image-size">
                     <div class="popup-gallery">
-                        <div class="col-sm-3 col-xs-6 hover01">
+                        <div class="col-md-3 col-sm-4 col-xs-6 hover01">
                             <a href="{{ asset('img/superbox-full-10.jpg') }}">
                                 <img src="{{ asset('img/superbox-full-10.jpg') }}">
                             </a>
                         </div>
-                        <div class="col-sm-3 col-xs-6 hover01">
+                        <div class="col-md-3 col-sm-4 col-xs-6 hover01">
                             <a href="{{ asset('img/superbox-full-3.jpg') }}">
                                 <img class="hover01" src="{{ asset('img/superbox-full-3.jpg') }}">
                             </a>
                         </div>
-                        <div class="col-sm-3 col-xs-6 hover01">
-                            <a href="{{ asset('img/superbox-full-14.jpg') }}">
-                                <img src="{{ asset('img/superbox-full-14.jpg') }}">
-                            </a>
-                        </div>
-                        <div class="col-sm-3 col-xs-6 hover01">
+                        <div class="col-md-3 col-sm-4 col-xs-6 hover01">
                             <a href="{{ asset('img/superbox-full-9.jpg') }}">
                                 <img src="{{ asset('img/superbox-full-9.jpg') }}">
+                            </a>
+                        </div>
+                        <div class="col-md-3 col-sm-4 col-xs-6 hover01">
+                            <a href="{{ asset('img/superbox-full-14.jpg') }}">
+                                <img src="{{ asset('img/superbox-full-14.jpg') }}">
                             </a>
                         </div>
                     </div>
