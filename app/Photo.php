897b9713914252bdfa918a7bf2af1f7c725eb9dd
Evgeny <tonitsoi.evgenii@gmail.com>
2018-04-12 20:31:48 +0300

    Defining relationships

--- app/Photo.php
+++ app/Photo.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Photo extends Model
-{
-    //
+class Photo extends Model {
+
+   public function portfolios() {
+      return $this->belongsToMany('App\Portfolio');
+   }
+
 }
--- app/Portfolio.php
+++ app/Portfolio.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Portfolio extends Model
-{
-    //
+class Portfolio extends Model {
+
+   public function photos() {
+      return $this->belongsToMany('App\Photo');
+   }
+
 }
