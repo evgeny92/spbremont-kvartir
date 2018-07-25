a6c318e7770ca79489938942f4a13684b7dfee04
Evgeny <tonitsoi.evgenii@gmail.com>
2018-07-25 22:34:19 +0300

    Add Yandex Metrika

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -4,5 +4,6 @@
 <script src="{{ asset('js/custom.js') }}"></script>
 <script src="{{ asset('js/jquery.easing.js') }}"></script>
 <script async src="https://usocial.pro/usocial/usocial.js?v=6.1.4" data-script="usocial" charset="utf-8"></script>
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter49749817 = new Ya.Metrika2({ id:49749817, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/49749817" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 @yield('scripts')
 
