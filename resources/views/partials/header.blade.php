fe87e4e83b88fe8779cb7aeddb6c0ceace47fa8c
Evgeny <tonitsoi.evgenii@gmail.com>
2018-07-25 22:05:12 +0300

    Yandex verification

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="yandex-verification" content="396ab79a7a34fea2" />
     @yield('meta-tags')
     <meta property="og:type" content=website">
     <meta property="og:site_name" content="">
